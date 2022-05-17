63ca0595a918efdec1c78a840d4b0c793d384174
njcinnamond <njcinnamond@gmail.com>
2022-05-16 20:50:02 -0400

    first commit of server

---
+++ server/routes/record.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+// recordRoutes is an instance of the express router.
+// We use it to define our routes.
+// The router will be added as a middleware and will take control of requests starting with path /listings.
+const recordRoutes = express.Router();
+
+// This will help us connect to the database
+const dbo = require('../db/conn');
+
+module.exports = recordRoutes;
